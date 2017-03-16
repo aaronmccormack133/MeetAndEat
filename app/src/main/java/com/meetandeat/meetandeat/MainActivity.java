6597cab1c2564d6e770ecf95516d7b95bfd77b50
Aaron Mc Cormack <x15533283@student.ncirl.ie>
2017-03-16 13:57:50 +0000

    Basic Navigation Completed

--- app/src/main/java/com/meetandeat/meetandeat/MainActivity.java
+++ app/src/main/java/com/meetandeat/meetandeat/MainActivity.java
@@ -10,21 +10,21 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 //http://blog.iamsuleiman.com/using-bottom-navigation-view-android-design-support-library/
 //https://github.com/1priyank1/BottomNavigation-Demo
-//
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textProfiles;
-    private TextView textHome;
-    private TextView textMessenger;
-
     ViewPager myPager;
+    BottomNavigationView bottomNavigationView;
+    MenuItem prevMenuItem;
+    ProfileFragment profileFragment;
+    MessageFragment messageFragment;
+    HomeFragment homeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,62 +35,75 @@ public class MainActivity extends AppCompatActivity {
         myPager = (ViewPager) findViewById(R.id.Pager);
         myPager.setAdapter(new SamplePagerAdapter(getSupportFragmentManager()));
 
+        //navigation
+        bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
 
-        //Navigation
-        textHome = (TextView) findViewById(R.id.text_home);
-        textProfiles = (TextView) findViewById(R.id.text_profiles);
-        textMessenger = (TextView) findViewById(R.id.text_messenger);
-
-//        BottomNavigationView bottomNavigationView = (BottomNavigationView)
-//           findViewById(R.id.bottom_navigation);
-/*
         bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener(){
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Nullable
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item){
-                        switch(item.getItemId()){
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
                             case R.id.action_messenger:
-                                //textHome.setVisibility(View.GONE);
-                                //textMessenger.setVisibility(View.VISIBLE);
-                                //textProfiles.setVisibility(View.GONE);
+                                myPager.setCurrentItem(0);
                                 break;
                             case R.id.action_restaurants:
-                                //textHome.setVisibility(View.VISIBLE);
-                                //textMessenger.setVisibility(View.GONE);
-                                //textProfiles.setVisibility(View.GONE);
+                                myPager.setCurrentItem(1);
                                 break;
                             case R.id.action_profiles:
-                                //textHome.setVisibility(View.GONE);
-                                //textMessenger.setVisibility(View.GONE);
-                                //textProfiles.setVisibility(View.VISIBLE);
+                                myPager.setCurrentItem(2);
                                 break;
                         }
                         return true;
                     }
                 }
         );
-*/
+
+        myPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: " + position);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
-    public class SamplePagerAdapter extends FragmentPagerAdapter{
-        public SamplePagerAdapter(FragmentManager fragM){
+    public class SamplePagerAdapter extends FragmentPagerAdapter {
+        public SamplePagerAdapter(FragmentManager fragM) {
             super(fragM);
         }
+
         @Override
-        public Fragment getItem(int position){
-            if(position == 0){
+        public Fragment getItem(int position) {
+            if (position == 0) {
                 return new MessageFragment();
 
-            }
-            else if(position == 1){
+            } else if (position == 1) {
                 return new HomeFragment();
-            }
-            else{
+            } else {
                 return new ProfileFragment();
             }
         }
+
         @Override
-        public int getCount(){
+        public int getCount() {
             return 3;
         }
     }
