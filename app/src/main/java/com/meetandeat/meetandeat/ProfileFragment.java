dc3e8d2ac3e415ee57ec4ec6fefa36a4b87e3b0e
zakoleary <zakoleary@gmail.com>
2017-04-18 02:46:55 +0100

    Profile Spinner Added

--- app/src/main/java/com/meetandeat/meetandeat/ProfileFragment.java
+++ app/src/main/java/com/meetandeat/meetandeat/ProfileFragment.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.support.v4.app.Fragment;
+import android.widget.Toast;
 
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 
 /**
@@ -14,6 +19,30 @@ import android.support.v4.app.Fragment;
  */
 
 public class ProfileFragment extends Fragment {
+    Spinner spinner;
+    ArrayAdapter <CharSequence> adapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.ProfileFragment);
+        spinner = (Spinner)findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource( this,R.array.select_hobbies,android.R.layout.simple_spinner_item );
+        adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+        spinner.setAdapter( adapter );
+        spinner.setOnItemSelectedListener( new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText( getApplicationContext(),parent.getItemAtPosition(position)+"selected", Toast.LENGTH_LONG ).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        } );
+
+    }
 
     public ProfileFragment(){
     }
