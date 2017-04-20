2f81f6124ca72fc7f798b6882872227aead3aea5
zakoleary <zakoleary@gmail.com>
2017-04-20 05:48:27 +0100

    home page/login page redone

--- app/src/main/java/com/meetandeat/meetandeat/MainActivity.java
+++ app/src/main/java/com/meetandeat/meetandeat/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
